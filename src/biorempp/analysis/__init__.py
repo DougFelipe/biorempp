531d0f7b07cc6a913812dd1bdd75710a7b3be972
Douglas <dougbti2022@gmail.com>
2025-07-08 12:29:33 -0300

    refactor: update GenePathwayAnalyzer import and remove obsolete modules

--- src/biorempp/analysis/__init__.py
+++ src/biorempp/analysis/__init__.py
@@ -1,5 +1,5 @@
 """Analysis module for BioRemPP."""
 
-from .gene_pathway_analysis import GenePathwayAnalyzer
+from .gene_pathway_analysis_processing import GenePathwayAnalyzer
 
 __all__ = ["GenePathwayAnalyzer"]
